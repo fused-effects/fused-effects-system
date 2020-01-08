0fe99b91d7fa3570996c65c368135c7dd8235a44
Rob Rix <rob.rix@me.com>
2020-01-08 17:34:28 -0500

    Use the same definition of Timings in both carriers.

--- src/Control/Carrier/Profile/Flat.hs
+++ src/Control/Carrier/Profile/Flat.hs
@@ -2,8 +2,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -24,22 +22,14 @@ module Control.Carrier.Profile.Flat
 , module Control.Effect.Profile
 ) where
 
-import           Control.Algebra
-import           Control.Carrier.Lift
-import           Control.Carrier.Writer.Strict
-import           Control.Effect.Profile
-import           Control.Monad.IO.Class
-import           Data.Coerce
-import qualified Data.HashMap.Strict as HashMap
-import           Data.List (sortOn)
-import           Data.Ord (Down(..))
-import           Data.Text (Text)
-import           Data.Text.Prettyprint.Doc
-import           Data.Text.Prettyprint.Doc.Render.Terminal
-import           Data.Time.Clock
-import           Numeric (showFFloat)
-import           Prelude hiding (lookup, sum)
-import           System.IO (stderr)
+import Control.Algebra
+import Control.Carrier.Lift
+import Control.Carrier.Writer.Strict
+import Control.Effect.Profile
+import Control.Monad.IO.Class
+import Data.Time.Clock
+import Data.Timing
+import Prelude hiding (lookup, sum)
 
 runProfile :: ProfileC m a -> m (Timings, a)
 runProfile (ProfileC m) = runWriter m
@@ -66,54 +56,4 @@ instance (Has (Lift IO) sig m, Effect sig) => Algebra (Profile :+: sig) (Profile
       k a
     R other -> ProfileC (send (handleCoercible other))
     where
-    timing ls t = Timings (HashMap.singleton ls (Timing t t t 1))
-
-
-type Label = Text
-
-data Timing = Timing
-  { sum   :: !NominalDiffTime
-  , min'  :: !NominalDiffTime
-  , max'  :: !NominalDiffTime
-  , count :: {-# UNPACK #-} !Int
-  }
-
-instance Semigroup Timing where
-  Timing s1 mn1 mx1 c1 <> Timing s2 mn2 mx2 c2 = Timing (s1 + s2) (mn1 `min` mn2) (mx1 `max` mx2) (c1 + c2)
-
-instance Monoid Timing where
-  mempty = Timing 0 0 0 0
-
-renderTiming :: Timing -> Doc AnsiStyle
-renderTiming t@Timing{ min', max' } = table (map go fields) <> line
-    where
-    table = group . encloseSep (flatAlt "{ " "{") (flatAlt " }" "}") ", "
-    fields =
-      [ (annotate (colorDull Green) "min", prettyMS min')
-      , (annotate (colorDull Green) "mean", prettyMS (mean t))
-      , (annotate (colorDull Green) "max", prettyMS max')
-      ]
-    go (k, v) = k <> colon <+> v
-    prettyMS = (<> annotate (colorDull White) "ms") . pretty . ($ "") . showFFloat @Double (Just 3) . (* 1000) . realToFrac
-
-mean :: Timing -> NominalDiffTime
-mean Timing{ sum, count } = sum / fromIntegral count
-
-
-newtype Timings = Timings { unTimings :: HashMap.HashMap Label Timing }
-
-instance Semigroup Timings where
-  Timings t1 <> Timings t2 = Timings (HashMap.unionWith (<>) t1 t2)
-
-instance Monoid Timings where
-  mempty = Timings mempty
-
-lookup :: Label -> Timings -> Maybe Timing
-lookup = coerce @(Label -> HashMap.HashMap Label Timing -> _) HashMap.lookup
-
-renderTimings :: Timings -> Doc AnsiStyle
-renderTimings (Timings ts) = vsep (map go (sortOn (Down . mean . snd) (HashMap.toList ts))) where
-  go (k, v) = annotate (color Green) (pretty k) <> pretty ':' <> softline <> renderTiming v
-
-reportTimings :: Has (Lift IO) sig m => Timings -> m ()
-reportTimings = sendM . renderIO stderr . layoutPretty defaultLayoutOptions . (<> line) . renderTimings
+    timing l t = singleton l (Timing t t t 1 mempty)
