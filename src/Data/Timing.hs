ab9799696db0537d077044b3238fb1a9399e395d
Rob Rix <rob.rix@me.com>
2020-01-25 16:38:14 -0500

    Inline a bunch of Timings-related things.

--- src/Data/Timing.hs
+++ src/Data/Timing.hs
@@ -38,9 +38,11 @@ data Timing = Timing
 
 instance Semigroup Timing where
   Timing s1 mn1 mx1 c1 sb1 <> Timing s2 mn2 mx2 c2 sb2 = Timing (s1 + s2) (mn1 `min` mn2) (mx1 `max` mx2) (c1 + c2) (sb1 <> sb2)
+  {-# INLINE (<>) #-}
 
 instance Monoid Timing where
   mempty = Timing 0 0 0 0 mempty
+  {-# INLINE mempty #-}
 
 renderTiming :: Timing -> Doc AnsiStyle
 renderTiming t@Timing{ min', max', sub } = table (map go fields) <> if null (unTimings sub) then mempty else nest 2 (line <> renderTimings sub)
@@ -56,26 +58,32 @@ renderTiming t@Timing{ min', max', sub } = table (map go fields) <> if null (unT
 
 mean :: Timing -> NominalDiffTime
 mean Timing{ sum, count } = sum / fromIntegral count
+{-# INLINE mean #-}
 
 
 type Label = Text
 
 label :: String -> Label
 label = pack
+{-# INLINE label #-}
 
 newtype Timings = Timings { unTimings :: HashMap.HashMap Label Timing }
 
 instance Semigroup Timings where
   Timings t1 <> Timings t2 = Timings (HashMap.unionWith (<>) t1 t2)
+  {-# INLINE (<>) #-}
 
 instance Monoid Timings where
   mempty = Timings mempty
+  {-# INLINE mempty #-}
 
 singleton :: Label -> Timing -> Timings
 singleton = coerce @(Label -> Timing -> _) HashMap.singleton
+{-# INLINE singleton #-}
 
 lookup :: Label -> Timings -> Maybe Timing
 lookup = coerce @(Label -> HashMap.HashMap Label Timing -> _) HashMap.lookup
+{-# INLINE lookup #-}
 
 renderTimings :: Timings -> Doc AnsiStyle
 renderTimings (Timings ts) = vsep (map go (sortOn (Down . mean . snd) (HashMap.toList ts))) where
