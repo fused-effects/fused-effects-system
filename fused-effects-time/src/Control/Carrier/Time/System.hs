ec9b8e620570e300348153de21aa64ebd03bcb5e
Rob Rix <rob.rix@me.com>
2020-07-20 13:01:14 -0400

    Define a MonadTrans instance for TimeC.

--- fused-effects-time/src/Control/Carrier/Time/System.hs
+++ fused-effects-time/src/Control/Carrier/Time/System.hs
@@ -11,6 +11,10 @@ import Control.Effect.Time
 import Control.Monad (MonadPlus)
 import Control.Monad.Fix (MonadFix)
 import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Trans.Class (MonadTrans(..))
 
 newtype TimeC m a = TimeC { runTime :: m a }
   deriving (Alternative, Applicative, Functor, Monad, MonadFail, MonadFix, MonadIO, MonadPlus)
+
+instance MonadTrans TimeC where
+  lift = TimeC
