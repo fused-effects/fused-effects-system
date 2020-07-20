9dd72d39af5216b71366f9cf455d323746a4cb15
Rob Rix <rob.rix@me.com>
2020-07-20 13:04:19 -0400

    Define an Algebra instance for System.TimeC.

--- fused-effects-time/src/Control/Carrier/Time/System.hs
+++ fused-effects-time/src/Control/Carrier/Time/System.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Carrier.Time.System
 ( -- * Time carrier
   TimeC(..)
@@ -6,12 +11,15 @@ module Control.Carrier.Time.System
 , module Control.Effect.Time
 ) where
 
+import Control.Algebra
 import Control.Applicative (Alternative)
+import Control.Carrier.Lift
 import Control.Effect.Time
 import Control.Monad (MonadPlus)
 import Control.Monad.Fix (MonadFix)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Trans.Class (MonadTrans(..))
+import Data.Time.Clock.System
 
 newtype TimeC m a = TimeC { runTime :: m a }
   deriving (Alternative, Applicative, Functor, Monad, MonadFail, MonadFix, MonadIO, MonadPlus)
@@ -19,3 +27,9 @@ newtype TimeC m a = TimeC { runTime :: m a }
 instance MonadTrans TimeC where
   lift = TimeC
   {-# INLINE lift #-}
+
+instance Has (Lift IO) sig m => Algebra (Time SystemTime :+: sig) (TimeC m) where
+  alg hdl sig ctx = case sig of
+    L Now   -> (<$ ctx) <$> sendIO getSystemTime
+    R other -> TimeC (alg (runTime . hdl) other ctx)
+  {-# INLINE alg #-}
