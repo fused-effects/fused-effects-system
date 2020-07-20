fedc8ae230714185111e4faeb6dc33bda185e0a5
Rob Rix <rob.rix@me.com>
2020-07-20 13:22:18 -0400

    Define a timeWith operation computing a delta on two instants.

--- fused-effects-time/src/Control/Effect/Time.hs
+++ fused-effects-time/src/Control/Effect/Time.hs
@@ -3,6 +3,7 @@
 module Control.Effect.Time
 ( -- * Time effect
   now
+, timeWith
 , Time(..)
   -- * Re-exports
 , Algebra
@@ -17,5 +18,10 @@ now :: Has (Time instant) sig m => m instant
 now = send Now
 {-# INLINE now #-}
 
+timeWith :: Has (Time instant) sig m => (instant -> instant -> delta) -> m a -> m (delta, a)
+timeWith with m = send (TimeWith with m)
+{-# INLINE timeWith #-}
+
 data Time instant (m :: Type -> Type) k where
-  Now :: Time instant m instant
+  Now      ::                                         Time instant m instant
+  TimeWith :: (instant -> instant -> delta) -> m a -> Time instant m (delta, a)
