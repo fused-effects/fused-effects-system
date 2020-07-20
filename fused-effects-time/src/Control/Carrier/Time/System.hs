82a93b112d6703b0215e7367cea72a0f6e1948c9
Rob Rix <rob.rix@me.com>
2020-07-20 12:58:23 -0400

    Re-export the Time effect.

--- fused-effects-time/src/Control/Carrier/Time/System.hs
+++ fused-effects-time/src/Control/Carrier/Time/System.hs
@@ -1,6 +1,10 @@
 module Control.Carrier.Time.System
 ( -- * Time carrier
   TimeC(..)
+  -- * Time effect
+, module Control.Effect.Time
 ) where
+
+import Control.Effect.Time
 
 newtype TimeC m a = TimeC { runTime :: m a }
