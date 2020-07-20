a9dd22ed4e52367693656312bb56cd5fcfd96d21
Rob Rix <rob.rix@me.com>
2020-07-20 12:59:52 -0400

    Derive a bunch of instances.

--- fused-effects-time/src/Control/Carrier/Time/System.hs
+++ fused-effects-time/src/Control/Carrier/Time/System.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Time.System
 ( -- * Time carrier
   TimeC(..)
@@ -5,6 +6,11 @@ module Control.Carrier.Time.System
 , module Control.Effect.Time
 ) where
 
+import Control.Applicative (Alternative)
 import Control.Effect.Time
+import Control.Monad (MonadPlus)
+import Control.Monad.Fix (MonadFix)
+import Control.Monad.IO.Class (MonadIO)
 
 newtype TimeC m a = TimeC { runTime :: m a }
+  deriving (Alternative, Applicative, Functor, Monad, MonadFail, MonadFix, MonadIO, MonadPlus)
