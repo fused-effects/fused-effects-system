6e30af46bd28554bcbdd5b9a30a0ab7800af6dc7
Rob Rix <rob.rix@me.com>
2020-07-20 13:32:26 -0400

    Revert "Define a timeWith operation computing a delta on two instants."
    This reverts commit fedc8ae230714185111e4faeb6dc33bda185e0a5.

--- fused-effects-time/src/Control/Effect/Time.hs
+++ fused-effects-time/src/Control/Effect/Time.hs
@@ -3,7 +3,6 @@
 module Control.Effect.Time
 ( -- * Time effect
   now
-, timeWith
 , Time(..)
   -- * Re-exports
 , Algebra
@@ -18,10 +17,5 @@ now :: Has (Time instant) sig m => m instant
 now = send Now
 {-# INLINE now #-}
 
-timeWith :: Has (Time instant) sig m => (instant -> instant -> delta) -> m a -> m (delta, a)
-timeWith with m = send (TimeWith with m)
-{-# INLINE timeWith #-}
-
 data Time instant (m :: Type -> Type) k where
-  Now      ::                                         Time instant m instant
-  TimeWith :: (instant -> instant -> delta) -> m a -> Time instant m (delta, a)
+  Now :: Time instant m instant
