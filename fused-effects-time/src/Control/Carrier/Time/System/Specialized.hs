e41f9ce2aa65bfe32ee6b0ef90293d293cf27cea
Rob Rix <rob.rix@me.com>
2020-07-21 07:55:41 -0400

    Define a type-restricted sinceEpochWith.

--- fused-effects-time/src/Control/Carrier/Time/System/Specialized.hs
+++ fused-effects-time/src/Control/Carrier/Time/System/Specialized.hs
@@ -3,11 +3,12 @@ module Control.Carrier.Time.System.Specialized
 , now
 , timeWith
 , epoch
+, sinceEpochWith
 , eraFrom
 ) where
 
 import Control.Algebra
-import Control.Carrier.Time.System hiding (epoch, eraFrom, now, timeWith)
+import Control.Carrier.Time.System hiding (epoch, eraFrom, now, sinceEpochWith, timeWith)
 
 now :: Has (Time Instant) sig m => m Instant
 now = send Now
@@ -25,6 +26,14 @@ timeWith with m = do
 epoch :: Has (Time Instant) sig m => m Instant
 epoch = send Epoch
 {-# INLINE epoch #-}
+
+sinceEpochWith :: Has (Time Instant) sig m => (Instant -> Instant -> delta) -> m delta
+sinceEpochWith with = do
+  now <- now
+  epoch <- epoch
+  let d = with epoch now
+  d `seq` pure d
+{-# INLINE sinceEpochWith #-}
 
 eraFrom :: Has (Time Instant) sig m => Instant -> m a -> m a
 eraFrom t m = send (EraFrom t m)
