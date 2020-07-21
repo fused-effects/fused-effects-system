930609d2152b19f3fbae534744d847a017cbe5ed
Rob Rix <rob.rix@me.com>
2020-07-21 07:24:15 -0400

    Hide the unspecialized operations.

--- fused-effects-time/src/Control/Carrier/Time/System/Specialized.hs
+++ fused-effects-time/src/Control/Carrier/Time/System/Specialized.hs
@@ -2,4 +2,4 @@ module Control.Carrier.Time.System.Specialized
 ( module Control.Carrier.Time.System
 ) where
 
-import Control.Carrier.Time.System
+import Control.Carrier.Time.System hiding (epoch, eraFrom, now, timeWith)
