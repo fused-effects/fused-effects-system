750b420727f266feadb42ef60f6b31e8d424c9f4
Rob Rix <rob.rix@me.com>
2020-07-21 07:48:24 -0400

    Define a type-restricted timeWith.

--- fused-effects-time/src/Control/Carrier/Time/System/Specialized.hs
+++ fused-effects-time/src/Control/Carrier/Time/System/Specialized.hs
@@ -1,15 +1,25 @@
 module Control.Carrier.Time.System.Specialized
 ( module Control.Carrier.Time.System
 , now
+, timeWith
 , epoch
 ) where
 
 import Control.Algebra
-import Control.Carrier.Time.System hiding (epoch, now)
+import Control.Carrier.Time.System hiding (epoch, now, timeWith)
 
 now :: Has (Time Instant) sig m => m Instant
 now = send Now
 {-# INLINE now #-}
+
+timeWith :: Has (Time Instant) sig m => (Instant -> Instant -> delta) -> m a -> m (delta, a)
+timeWith with m = do
+  start <- now
+  a <- m
+  end <- now
+  let d = with start end
+  d `seq` pure (d, a)
+{-# INLINE timeWith #-}
 
 epoch :: Has (Time Instant) sig m => m Instant
 epoch = send Epoch
