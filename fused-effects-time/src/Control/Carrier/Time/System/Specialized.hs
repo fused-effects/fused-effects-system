59cde33bff742699a678d6cad4611958036a6016
Rob Rix <rob.rix@me.com>
2020-07-21 07:24:47 -0400

    Define a type-restricted now.

--- fused-effects-time/src/Control/Carrier/Time/System/Specialized.hs
+++ fused-effects-time/src/Control/Carrier/Time/System/Specialized.hs
@@ -1,5 +1,11 @@
 module Control.Carrier.Time.System.Specialized
 ( module Control.Carrier.Time.System
+, now
 ) where
 
+import Control.Algebra
 import Control.Carrier.Time.System hiding (epoch, eraFrom, now, timeWith)
+
+now :: Has (Time Instant) sig m => m Instant
+now = send Now
+{-# INLINE now #-}
